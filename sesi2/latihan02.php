ae39e2b1a15a5ec7d82bd70d3e343b7d6ae0444a
OktaWidyastuti <127836397+OktaWidyastuti@users.noreply.github.com>
2023-03-25 08:29:41 +0700

    upload file

---
+++ sesi2/latihan02.php
@@ -0,0 +1,15 @@
+<?php
+    $angka = 3;
+
+    echo "Nilai Angka: <strong>".$angka;"</strong>";
+
+    if($angka > 4){
+        //kondisi true
+        echo "<br>Nilai Angka Lebih Dari 4";
+    }else{
+        //kondisi false
+        echo "<br>Nilai Angka Kurang Dari Sama Dengan $";
+    }
+        
+
+    
